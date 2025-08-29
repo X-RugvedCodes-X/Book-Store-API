989efcfc58e04e142742e6f621c9424b611e3f76
X-RugvedCodes-X <rugvedraorane14@gmail.com>
2025-08-30 03:38:49 +0530

    Update database/db.js

--- database/db.js
+++ database/db.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_CONNECTION_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGODB_CONNECTION_URL);
     console.log("MongoDB Connected Successfully !");
   } catch(err) {
     console.error("MongoDB connection failed:", err);
